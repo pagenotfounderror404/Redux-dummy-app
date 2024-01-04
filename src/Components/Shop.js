23da3b69d25760861aa19d835a12455e5fd37a48
Ayush Chakraborty <Ayush.Chakraborty@gds.ey.com>
2024-01-04 19:02:58 +0530

    Redux implemented

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,12 +1,40 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { actionCreators } from '../State/index'
+import { bindActionCreators } from 'redux'
 
 const Shop = () => {
+  const dispatch = useDispatch()
+  const actions = bindActionCreators(actionCreators, dispatch)
   return (
     <div>
-      <h2>Buy Adibas products -Rs. 50</h2>
-      <button className="btn btn-primary mx-2">-</button>
+      <h2>Deposit/Withdraw Money</h2>
+      <button
+        className="btn btn-primary mx-2"
+        onClick={() => actions.withdrawMoney(100)}
+      >
+        -
+      </button>
       Add this item to cart
-      <button className="btn btn-primary mx-2">+</button>
+      <button
+        className="btn btn-primary mx-2"
+        onClick={() => actions.depositMoney(100)}
+      >
+        +
+      </button>
+      {/* <button
+        className="btn btn-primary mx-2"
+        onClick={() => dispatch(actionCreators.withdrawMoney(100))}
+      >
+        -
+      </button>
+      Add this item to cart
+      <button
+        className="btn btn-primary mx-2"
+        onClick={() => dispatch(actionCreators.depositMoney(100))}
+      >
+        +
+      </button> */}
     </div>
   )
 }
---
+++ src/State/reducer/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import amountreducer from './amountreducer'
+const reducers = combineReducers({
+  amount: amountreducer,
+})
+export default reducers
