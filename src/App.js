bc5eaa776de67efbef11e6b1d02ba7f537a807e3
Ayush Chakraborty <Ayush.Chakraborty@gds.ey.com>
2024-01-04 15:25:13 +0530

    Dummy UI

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './Components/Navbar'
+import Shop from './Components/Shop'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Shop />
+    </div>
+  )
+}
+
+export default App
